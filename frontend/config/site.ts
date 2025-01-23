333e0b0b4835191fd6a5dbe51c64f3dcb02512b1
peplxx <seregaopkymelnikov@yandex.ru>
2025-01-23 22:27:16 +0300

    initial commit

---
+++ frontend/config/site.ts
@@ -0,0 +1,9 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Next-Template",
+  description: "Make websites faster and better.",
+  links: {
+    github: "https://github.com/peplxx/next-template",
+  },
+};
---
+++ frontend/pages/index.tsx
@@ -0,0 +1,10 @@
+import DefaultLayout from "@/layouts/default";
+
+export default function IndexPage() {
+  return (
+    <DefaultLayout>
+      <h2>Hello, This is Next-Template</h2>
+      <a href="https://github.com/peplxx/next-template"> Github link</a>
+    </DefaultLayout>
+  );
+}
